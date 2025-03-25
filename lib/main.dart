68efb9deedfc06f13e144d55250a114255c0c8ec
GGeonkim <rlarjsdlekd@naver.com>
2025-03-26 00:24:27 +0900

    시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cafe_order_system/home.dart';
+import 'package:cafe_order_system/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo2222', home: const Home());
+    return LoginScreen();
   }
 }
