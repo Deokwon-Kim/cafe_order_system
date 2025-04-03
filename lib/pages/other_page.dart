383fcac74ee3bf8e3b3e663ddee646d05e7ae231
GGeonkim <rlarjsdlekd@naver.com>
2025-04-03 14:39:13 +0900

    상세 페이지, 주문 내역 페이지 및 개인 정보 페이지 파일 추가 그리고 상세페이지 이동 기능 구현현

--- lib/pages/other_page.dart
+++ lib/pages/other_page.dart
@@ -11,12 +11,30 @@ class OtherPage extends StatelessWidget {
     final username = Provider.of<UserProvider>(context).username;
 
     final List<Widget> gridItems = [
-      menuItem('별 히스토리', Color(0xfff37210), Icon(Icons.star_border)),
-      menuItem('전자 내역', Color(0xfff37210), Icon(Icons.receipt_long)),
-      menuItem('주문 내역', Color(0xfff37210), Icon(Icons.receipt_long)),
-      menuItem('개인정보 관리', Color(0xfff37210), Icon(Icons.receipt_long)),
-      menuItem('계정정보', Color(0xfff37210), Icon(Icons.person)),
-      menuItem('나만의 메뉴', Color(0xfff37210), Icon(Icons.notifications)),
+      menuItem(
+        context,
+        '별 히스토리',
+        Color(0xfff37210),
+        Icon(Icons.star_border),
+        'star_detail',
+      ),
+      //menuItem('전자 내역', Color(0xfff37210), Icon(Icons.receipt_long)),
+      menuItem(
+        context,
+        '주문 내역',
+        Color(0xfff37210),
+        Icon(Icons.receipt_long),
+        'order_history',
+      ),
+      // menuItem('개인정보 관리', Color(0xfff37210), Icon(Icons.receipt_long)),
+      menuItem(
+        context,
+        '계정정보',
+        Color(0xfff37210),
+        Icon(Icons.person),
+        'personal_info',
+      ),
+      //menuItem('나만의 메뉴', Color(0xfff37210), Icon(Icons.notifications)),
     ];
 
     return Scaffold(
@@ -64,9 +82,17 @@ class OtherPage extends StatelessWidget {
   }
 }
 
-Widget menuItem(String title, Color color, Icon icon) {
+Widget menuItem(
+  BuildContext context,
+  String title,
+  Color color,
+  Icon icon,
+  String routeName,
+) {
   return GestureDetector(
-    onTap: () {},
+    onTap: () {
+      Navigator.pushNamed(context, routeName);
+    },
     child: Container(
       decoration: BoxDecoration(color: Colors.white),
       padding: EdgeInsets.all(10),
